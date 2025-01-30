67a61df81aa778a091811d717f48d5dda65cde37
milind30082000 <milindphalke@Milinds-MacBook-Pro.local>
2025-01-30 11:14:59 +0530

    Chat Application Commit for Real Time changes

---
+++ src/main/java/com/chat/chatApp/model/ChatMessage.java
@@ -0,0 +1,41 @@
+package com.chat.chatApp.model;
+
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+
+    public ChatMessage(Long id, String sender, String content) {
+        this.id = id;
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public ChatMessage() {
+        //default constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
